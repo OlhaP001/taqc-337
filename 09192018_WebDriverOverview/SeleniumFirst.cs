d765a05ec60a19bc960d1abf50dd3a2537b411ae
Olha <olga.pavnyk@gmail.com>
2018-09-19 17:22:02 +0300

    Commit 1

---
+++ 09192018_WebDriverOverview/SeleniumFirst.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace _09192018_WebDriverOverview
+{
+    [TestFixture]
+    class SeleniumFirst
+    {
+        private IWebDriver driver;
+        [OneTimeSetUp] //Strt one time before all methods
+        public void BeforeAllMethods()
+        {
+            driver = new ChromeDriver();
+            //driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [SetUp] //Performong before every method
+        public void SetUp()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com.ua/");
+        }
+
+        [OneTimeTearDown] //Strt one time after all methods
+        public void AfterAllMethods()
+        {
+            driver.Quit(); //Close browser and stop server   
+        }
+
+        [Test]
+        public void TheATest()
+        {
+            //private IWebDriver driver;
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            //driver.Navigate().GoToUrl("https://www.google.com.ua/");
+            //
+            driver.FindElement(By.Id("lst-ib")).Click();
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("selenium ide" + Keys.Enter);
+            Thread.Sleep(2000);
+            //
+            //driver.FindElement(By.Id("mKlEF")).Click();
+            driver.FindElement(By.LinkText("Selenium IDE")).Click();
+            driver.FindElement(By.LinkText("Download")).Click();
+            Thread.Sleep(2000);
+            //
+            Assert.AreEqual("Selenium IDE is a Chrome and Firefox plugin which records and plays back user interactions with the browser. Use this to either create simple scripts or assist in exploratory testing.",
+                driver.FindElement(By.CssSelector("a[name=\"selenium_ide\"] > p")).Text);
+            //driver.Quit(); //Close browser and stop server
+            //driver.Close(); //Close just brovser
+        }
+    }
+}
---
+++ 09192018_WebDriverOverview/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace _09192018_WebDriverOverview
+{
+    //[TestClass] //Microsoft Test Framework attribute
+    [TestFixture] //NUnit framework attribute
+    //[Parallelizable(ParallelScope.All)]
+    public class UnitTest1
+    {
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //    Console.WriteLine("start");
+        //    //int i = 0;
+        //    double i = 0;
+        //    Console.WriteLine("(i+1)/i = " + (i + 1) / i);
+        //    Console.WriteLine("done"); //Print to console doesn't work on tests. Logs should be added
+        //    //throw new Exception("ha-ha-ha");
+        //}
+
+        //NUnit example
+        [OneTimeSetUp] //Strt one time before all methods
+        public void BeforeAllMethods()
+        {
+            Console.WriteLine("[OneTimeSetUp] BeforeAllMethods()");
+            //MessageBox.Show("[OneTimeSetUp] BeforeAllMethods()", "info",
+            //MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        [OneTimeTearDown] //Strt one time after all methods
+        public void AfterAllMethods()
+        {
+            Console.WriteLine("[OneTimeTearDown] AfterAllMethods()");
+            //MessageBox.Show("[OneTimeTearDown] AfterAllMethods()", "info",
+            //MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        [SetUp] //Performong before every method
+        public void SetUp()
+        {
+            Console.WriteLine("[SetUp] SetUp()");
+        }
+
+        [TearDown] //Performong after every method
+        public void TearDown()
+        {
+            Console.WriteLine("[TearDown] TearDown()");
+        }
+
+
+        //Next methods show how we can give parameters to the test
+        //[TestMethod] //Microsoft attribute
+        //[Test] //NUnit attribute
+        public void TestMethod1(
+            [Values("Hello", "World")]  string s,
+            [Random(1, 10, 5)] int x)
+        {
+            Console.WriteLine("Ok, s=" + s + " x=" + x);
+        }
+        //[TestCase(5, ExpectedResult = true)]
+        //[TestCase(-15, ExpectedResult = false)]
+        public bool TestMethod2(int x)
+        { return x > 0; }
+
+        // DataProvider
+        private static readonly object[] ConverterData =
+        {
+            new object[] { 65, 'A' },
+            new object[] { 97, 'a' },
+            new object[] { 98, 'b' }
+        };
+        //[TestCase(65, 'A')]
+        //[TestCase(97, 'a')]
+        [Test, TestCaseSource(nameof(ConverterData))]
+        public void TestMethod3(int x, char c)
+        {
+            char expexted = c;
+            char actual = (char)x;
+            Assert.AreEqual(expexted, actual);
+        }
+
+    }
+}
